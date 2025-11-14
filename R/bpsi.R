1e4438d8e35db88e648dec6140b8658b0e1d9464
tiagobchagas <tiagomcap@gmail.com>
2025-11-14 10:31:52 -0300

    bpsi changes

--- R/bpsi.R
+++ R/bpsi.R
@@ -8,40 +8,39 @@
 ##'
 ##' @param problist A list of object of class `probsup`, obtained from the [ProbBreed::prob_sup] function
 ##' @param increase Logical vector of amount of traits used in the same order of problist.`TRUE` (default) if the selection is for increasing the trait value, `FALSE` otherwise.
-##' @param omega A numeric representing the weight of each trait, the default is 1, the trait with more
+##' @param lambda A numeric representing the weight of each trait, the default is 1, the trait with more
 ##' economic interest should be greater.
 ##' @param int A numeric representing the selection intensity
 ##' (between 0 and 1)
 ##'##' @param save.df Logical. Should the data frames be saved in the work directory?
 ##' `TRUE` for saving, `FALSE` (default) otherwise.
-##' @param verbose A logical value. If `TRUE`, the function will indicate the
 ##' completed steps. Defaults to `FALSE`.
 ##'
 ##' @return The function returns an object of class `BPSI`, which contains two lists,
 ##' one with the `BPSI`- Bayesian Probabilistic Selection Index, and another with the original `data`-
-##' with across-environments probabilities for each trait.
+##' with across-environments probabilities of superior performance for each trait.
 ##'
 ##'
 ##'
 ##'
 ##' @details
-##' Probabilities provide the risk of recommending a selection candidate for a target
-##' population of environments or for a specific environment. `prob_sup`
-##' computes the probabilities of superior performance and the probabilities of superior stability:
+##' Probabilities provide the risk of recommending a selection candidate for a
+##'  multitrait ideotype in a target population of environments.
+##'   `BPSI` computes the probabilities of superior performance for multitrai selection:
 ##'
 ##' \itemize{\item Bayesian Probabilistic Selection Index}
 ##'
 ##'
 ##' \deqn{BPSI_i = \sum_{m=1}^{t} \frac{\gamma_{pt} -\gamma_{it} }{(1/\lambda_t)}}
 ##'
-##' where \eqn{\gamma_p} is the probability of superior performance of the worst genotype for the trait \eqn{m},
+##' where \eqn{\gamma_p} is the probability of superior performance of the worst genotype for the trait \eqn{t},
 ##' \eqn{\gamma} is the probability of superior performance of genotype  \eqn{i} for trait \eqn{t},
 ##'  \eqn{t} is the total number of traits evaluated,
 ##'   \eqn{\left(m = 1, 2, ..., t \right)},
 ##' and \eqn{\lambda} is the weight for each trait \eqn{t}.
 ##'
 ##'
-##'
+##' More details about the usage of `BPSI` can be found at \url{https://tiagobchagas.github.io/BPSI/}.
 ##'
 ##' @references
 ##'
@@ -72,13 +71,8 @@
 #' \donttest{
 ##'
 ##'
-##' library(ProbBreed)
-##' library(tidyverse)
-##'
-##' source("Data/bpsi_fun.R")
 ##' met_df=read.csv("https://raw.githubusercontent.com/tiagobchagas/BPSI/refs/heads/main/Data/blues_long.csv",header=T)
 ##'
-##' head(met_df)
 ##' mod = bayes_met(data = met_df,
 ##'                 gen = "gen",
 ##'                 loc = "env",
@@ -114,48 +108,36 @@
 ##'
 ##' models=list(mod,mod2,mod3)
 ##' names(models) <- c("PH","GY","NDM")
-##' outs= vector("list",length(models))
-##' names(outs) <- c("PH","GY","NDM")
-##'
-##' for (mods in 1:length(models)) {
-##'   a= extr_outs(model = models[[mods]],
-##'                probs = c(0.05, 0.95),
-##'                verbose = TRUE)
-##'   outs[[mods]]=a
-##'   rm(a)
-##' }
-##'
-##' results= vector("list",length(outs))
-##' names(results) <- c("PH","GY","NDM")
 ##' inc=c(FALSE,TRUE,FALSE)
+##' names(inc) <- names(models)
 ##'
-##' for (mods in 1:length(outs)) {
-##'   a = prob_sup(extr = outs[[mods]],
-##'                int = .2,
-##'                increase = inc[[mods]],
-##'                save.df = FALSE,
-##'                verbose = TRUE)
-##'   results[[mods]]=a
-##'   rm(a)
-##' }
-##' source("R/bpsi.R")
+##' results <- lapply(names(models), function(model_name) {
+##'   x <- models[[model_name]]  # actual model object
+##'
+##'   outs <- extr_outs(model = x,
+##'                     probs = c(0.05, 0.95),
+##'                     verbose = TRUE)
+##'
+##'   a <- prob_sup(extr = outs,
+##'                 int = .2,
+##'                 increase = inc[[model_name]],  # ← now model_name is a character!
+##'                 save.df = FALSE,
+##'                 verbose = TRUE)
+##'
+##'   return(a)
+##' })
+##' names(results) <- names(models)
 ##'
 ##'
-##' setdiff(results[[2]]$across$g_hpd$gen,results[[1]]$across$g_hpd$gen)
 ##'
-##' length(results[[3]]$across$g_hpd$gen)
-##'
-##' results[[2]]$across$perfo <- results[[2]]$across$perfo[-which(results[[2]]$across$perfo$ID %in% c("G10", "G36", "G37")),]
-##' results[[3]]$across$perfo <- results[[3]]$across$perfo[-which(results[[3]]$across$perfo$ID %in% c("G10", "G36", "G37")),]
 ##'
 ##'
 ##'
 ##' bpsi=BPSI(problist=results,
 ##'           increase = c(FALSE,TRUE,FALSE),
 ##'           int = 0.1,
-##'           omega=c(1,2,1),
-##'           save.df = F,
-##'           verbose = T)
+##'           lambda=c(1,2,1),
+##'           save.df = F)
 ##'
 ##'
 ##'
@@ -165,57 +147,73 @@
 ##'
 ##' df=print(bpsi)
 ##'
+##'
 #' }
 ##'
 
 
-BPSI = function(problist, increase = c(TRUE,FALSE,FALSE), omega, int, save.df = FALSE, verbose = FALSE){
 
-  stopifnot("Please, provide a valid vector of selection direction" !=is.null(increase))
-  stopifnot("Please, provide names to the list of models" !=is.null(names(problist)))
-  stopifnot("Please, provide a list of objects from class probsup" =class(problist[[1]])=="probsup")
-  stopifnot("Please provide a list with same number of genotypes for all traits" =
-              all(dim(problist[[1]]$across$perfo) == dim(problist[[2]]$across$perfo),
-                  dim(problist[[2]]$across$perfo) == dim(problist[[3]]$across$perfo)))
+BPSI = function(problist,increase, lambda, int, save.df = FALSE){
+
+  # stopifnot("Please, provide a valid vector of selection ideotype" !=is.null(increase))
+  stopifnot("Please, provide a list of objects from class probsup" = all(sapply(problist, inherits, "probsup")))
   stopifnot("Please, provide a valid selection intensity (number between 0 and 1)" = {
     is.numeric(int)
     int >= 0 & int <=1
   })
 
+  geno=sapply(problist,function(x){   length(x$across$perfo$ID)}  )
+  gen.min=which.min(geno)
+  traits=sapply(problist, function(x)  attr(x,"control")$trait  )
+  inc=sapply(problist, function(x) attr(x,"control")$increase)
+  traits=t(traits)
+  names(traits) <- traits
+  prob_l <- lapply(seq_along(problist), function(x){
+    problist[[x]]$across$perfo[which(problist[[x]]$across$perfo$ID %in% problist[[gen.min]]$across$perfo$ID),]
+  })
+  names(prob_l) <- names(traits)
 
-  df <- data.frame(matrix(nrow = length(problist[[1]]$across$perfo$ID),
-                          ncol = length(problist)))
-  rownames(df) <- problist[[1]]$across$perfo$ID
-  colnames(df) <- names(problist)
+
+  df <- data.frame(matrix(NA,nrow = length(prob_l[[1]]$ID),
+                          ncol = length(prob_l)))
+
+  rownames(df) <- prob_l[[1]]$ID
+  colnames(df) <- names(traits)
   rownames(df)=sort(row.names(df),decreasing = F)
-  # Preparation
-  for (traits in 1:length(problist)) {
-    tname=names(problist[traits])
-    a=problist[[traits]]$across$perfo[order(problist[[traits]]$across$perfo$ID,decreasing =F),]
-    df[paste0(tname)]=a$prob
-    rm(a)
-  }
 
-  if(is.null(omega)) omega=1 else omega=omega
-
-  bpsi <- apply(df, 2, function(x) {
-    rank(-x, ties.method = "min", na.last = "keep")
+  df[names(traits)] <- lapply(seq_along(prob_l), function(i){
+    prob_l[[i]]$prob[match(rownames(df), prob_l[[i]]$ID)]
   })
 
+  bpsi <- as.data.frame(apply(df, 2, function(x) {
+    rank(-x, ties.method = "min", na.last = "keep")
+  }))
 
-  bpsi <- apply(bpsi, 2, function(x) {
-    max(x) - x})
+  if(is.null(lambda)) lambda=1 else lambda=lambda
+  if(is.null(increase)) increase=inc else increase
+  if(all(increase==inc)) {
+    bpsi <- apply(bpsi, 2, function(x) {
+      max(x) - x})
+  }else{
+    bpsi <- sapply(seq_along(bpsi), function(x) {
+      if(increase[x]==TRUE){
+        max(bpsi[,x])-bpsi[,x]
+      }else{
+        bpsi[,x]-min(bpsi[,x]) } })
+    colnames(bpsi) <- colnames(df)
+    rownames(bpsi) <- row.names(df)
+  }
 
 
-  bpsi <- bpsi / (1/omega[col(bpsi)])
 
+
+  bpsi <- bpsi / (1/lambda[col(bpsi)])
 
   bpsi <- cbind(bpsi, rowSums(bpsi))
   colnames(bpsi)[ncol(bpsi)] <- "bpsi"
 
   bpsi <- as.data.frame(bpsi)
   gen_sel = bpsi[order(bpsi$bpsi,decreasing = T),]
-
   gen_i = round((length(gen_sel$bpsi) * int),0)
 
   selected=gen_sel[1:gen_i,]
@@ -223,14 +221,11 @@ BPSI = function(problist, increase = c(TRUE,FALSE,FALSE), omega, int, save.df = 
   bpsi$sel=ifelse(rownames(bpsi) %in% rownames(selected) , "Selected","Not_Selected")
   bpsi=bpsi[order(bpsi$bpsi,decreasing = T),]
   bpsi$gen=rownames(bpsi)
-  if(verbose) message('-> Genotypes selected using BPSI')
+  # if(verbose) message('-> Genotypes selected using BPSI')
+  message(paste0('-> BPSI selected ',int*100, "% of genotypes"))
   attr(bpsi, "control")=int
   output = list(BPSI=bpsi,df=df)
   class(output)="BPSI"
-
-  library(tidyverse)
-
-
 
 
 
@@ -245,7 +240,8 @@ BPSI = function(problist, increase = c(TRUE,FALSE,FALSE), omega, int, save.df = 
                      row.names = F)
   }
   # Final output -----------
-  if(verbose) message('Process completed!')
+  # if(verbose) message('Process completed!')
+  message('Process completed!')
   return(output)
 
 
@@ -260,7 +256,7 @@ BPSI = function(problist, increase = c(TRUE,FALSE,FALSE), omega, int, save.df = 
 #'
 #' @param problist A list of object of class `probsup`, obtained from the [ProbBreed::prob_sup] function
 ##' @param increase Logical vector of amount of traits used in the same order of problist.`TRUE` (default) if the selection is for increasing the trait value, `FALSE` otherwise.
-##' @param omega A numeric representing the weigth of each trait, the default is 1, the trait with more
+##' @param lambda A numeric representing the weigth of each trait, the default is 1, the trait with more
 ##' economic interest should be greater.
 ##' @param int A numeric representing the selection intensity
 ##' (between 0 and 1)
@@ -305,28 +301,77 @@ BPSI = function(problist, increase = c(TRUE,FALSE,FALSE), omega, int, save.df = 
 #' @rdname plot.probsup
 #' @export
 #'
-#' @examples
+##' @examples
 #' \donttest{
-##' source("Data/bpsi_fun.R")
 ##'
 ##'
-##' load("res_PL_year.rda")
-##' load("res_PH_year.rda")
-##' load("res_GY_year.rda")
+##' met_df=read.csv("https://raw.githubusercontent.com/tiagobchagas/BPSI/refs/heads/main/Data/blues_long.csv",header=T)
 ##'
-##' source("data/bpsi_fun.R")
+##' mod = bayes_met(data = met_df,
+##'                 gen = "gen",
+##'                 loc = "env",
+##'                 repl = NULL,
+##'                 trait = "PH",
+##'                 reg = NULL,
+##'                 year = NULL,
+##'                 res.het = T,
+##'                 iter =400, cores = 4, chain = 4) #recommended run at least 4k iterations
 ##'
-##' models= vector("list",length(3))
-##' models[[1]] = res_GY;
-##' models[[2]] = res_PH
-##' models[[3]] = res_PL
+##'
+##' mod2 = bayes_met(data = met_df,
+##'                  gen = "gen",
+##'                  loc = "env",
+##'                  repl = NULL,
+##'                  trait = "GY",
+##'                  reg = NULL,
+##'                  year = NULL,
+##'                  res.het = T,
+##'                  iter = 400, cores = 4, chain = 4) #recommended run at least 4k iterations
+##'
+##' mod3 = bayes_met(data = met_df,
+##'                  gen = "gen",
+##'                  loc = "env",
+##'                  repl =  NULL,
+##'                  trait = "NDM",
+##'                  reg = NULL,
+##'                  year = NULL,
+##'                  res.het = T,
+##'                  iter = 400, cores = 4, chain = 4) #recommended run at least 4k iterations
+##'
+##'
+##'
+##' models=list(mod,mod2,mod3)
+##' names(models) <- c("PH","GY","NDM")
+##' inc=c(FALSE,TRUE,FALSE)
+##' names(inc) <- names(models)
+##'
+##' results <- lapply(names(models), function(model_name) {
+##'   x <- models[[model_name]]  # actual model object
+##'
+##'   outs <- extr_outs(model = x,
+##'                     probs = c(0.05, 0.95),
+##'                     verbose = TRUE)
+##'
+##'   a <- prob_sup(extr = outs,
+##'                 int = .2,
+##'                 increase = inc[[model_name]],  # ← now model_name is a character!
+##'                 save.df = FALSE,
+##'                 verbose = TRUE)
+##'
+##'   return(a)
+##' })
+##' names(results) <- names(models)
+##'
+##'
+##'
+##'
+##'
 ##'
 ##' bpsi=BPSI(problist=results,
-##'  increase = c(TRUE,FALSE,FALSE),
-##'  int = 0.1,
-##'  omega=c(2,1,1),
-##'  save.df = F,
-##'  verbose = T)
+##'           increase = c(FALSE,TRUE,FALSE),
+##'           int = 0.1,
+##'           lambda=c(1,2,1),
+##'           save.df = F)
 ##'
 ##'
 ##'
@@ -334,13 +379,16 @@ BPSI = function(problist, increase = c(TRUE,FALSE,FALSE), omega, int, save.df = 
 ##'
 ##' plot(bpsi,category = "BPSI")
 ##'
-##' df=print(BPSI_soy)
+##' df=print(bpsi)
+##'
+##'
 #' }
 #'
 
 
 plot.BPSI = function(BPSI_result, ..., category = "BPSI"){
 
+  library(ggplot2)
   obj = BPSI_result
 
 
@@ -356,20 +404,21 @@ plot.BPSI = function(BPSI_result, ..., category = "BPSI"){
   #retrieve = function(x) do.call(rbind, strsplit(x, '@#_'))[,1]
   obj$gen=rownames(obj)
 
+  obj$id <- 1:nrow(obj)
+  obj$angle <- 90 - 360 * (obj$id - 0.5) / nrow(obj)
+  obj$hjust <-  ifelse(obj$angle < -90, 1, 0)
+  obj$angle = ifelse(obj$angle < -90, obj$angle + 180, obj$angle)
 
-  obj <- obj |>
-    mutate(id = row_number(),
-           angle = 90 - 360 * (id - 0.5) / n(),
-           hjust = ifelse(angle < -90, 1, 0),
-           angle = ifelse(angle < -90, angle + 180, angle))
 
   traits <- colnames(obj)[!colnames(obj) %in% c("bpsi","sel","gen", "angle", "hjust","id")]
   obja=obj
 
-  library(tidyverse)
-  library(ggrepel)
-  library(gghighlight)
-  obja=obj |> pivot_longer(paste(traits,sep = ":"),names_to = "trait",values_to = "rank")
+
+  obja <- reshape(obj, direction = "long",varying =list(traits),
+                  v.names = "rank",timevar = "trait",idvar="gen",times = traits )
+
+
+
 
   selected=obja[which(obja$sel %in% "Selected"),]
 
@@ -380,18 +429,10 @@ plot.BPSI = function(BPSI_result, ..., category = "BPSI"){
   # Rank plot --------------
   if(category == "Ranks"){
 
+    library(ggplot2)
 
-    # geom_bar(aes(x = as.factor(gen), y = max(rank) - rank, fill = "Selected"),
-    #          data = subset(obja, gen %in% selected$gen),
-    #          stat = "identity") +
-    # geom_bar(aes(x = as.factor(gen), y = max(rank) - rank, fill = "Not_Selected"),
-    #          data = subset(obja, !gen %in% selected$gen),
-    #          stat = "identity") +
     ggplot() +
       geom_col( aes(x = gen,y =rank, fill=sel),data=obja)+
-
-      # geom_hline(data = subset(obja, gen %in% selected$gen) %>% group_by(trait) %>% summarize(max_rank = max(rank)),
-      #            aes(yintercept = max_rank), linetype = "dashed", color = "red", size = 0.5) +
 
       facet_wrap(~trait, scales = "free_x") +
       theme(
@@ -435,8 +476,6 @@ plot.BPSI = function(BPSI_result, ..., category = "BPSI"){
                          name = NULL,
                          guide = "none") +
       coord_polar(start = 0) +
-      geom_segment(aes(x = id, xend = id, y = max(bpsi) - bpsi, yend = max(bpsi) + 5),  # Adjusted
-                   color = "grey100", linewidth = 0.3) +
       geom_text(aes(x = id, y = max(bpsi) + 10,  # Keep original positioning
                     label = gen, angle = angle, hjust = hjust),
                 size = 2.8) +
@@ -448,44 +487,6 @@ plot.BPSI = function(BPSI_result, ..., category = "BPSI"){
       labs(fill = NULL) +
       # Remove any axis labels that might show transformed values
       scale_y_continuous(labels = NULL)
-
-
-    # ggplot() +
-    #   geom_col(
-    #     aes(y = bpsi, x = reorder(gen,bpsi), fill = gen),
-    #     data = subset(obj, gen %in% selected$gen)
-    #   )+geom_col(
-    #     aes(y = bpsi, x = reorder(gen,bpsi)),
-    #     fill = "grey",
-    #     data = subset(obj, !gen %in% selected$gen)
-    #   ) +geom_hline(yintercept = max(selected$bpsi))+
-    #   scale_fill_manual(values = viridis::turbo(length(unique(selected$gen)))) +
-    #   theme_minimal() +
-    #   labs(x = "Generation", y = "BPSI", fill = "Generation") +
-    #   geom_label_repel(
-    #     data = subset(obj, gen %in% selected$gen),
-    #     aes(x = gen, y = bpsi, label = gen),  # Specify x and y aesthetics
-    #     fill = "lightblue", size = 2.25,
-    #     box.padding = 0.1, max.overlaps = 20,label.padding = 0.1,
-    #     label.size = 0.1,max.time = 1.0,
-    #     nudge_x = 7.0,nudge_y = 7.0,force=10,force_pull = 10,max.iter = 20000
-    #   ) +
-    #   gghighlight(gen %in% selected$gen) +
-    #   geom_hline(
-    #     aes(yintercept = max(bpsi, na.rm = TRUE), linetype = "BPSI"),  # Handle NA values
-    #     color = "black"
-    #   ) +
-    #   labs(
-    #     x = "Genotypes", y = "BPSI", , linetype = "Selected"
-    #   ) +guides(fill = "none")+
-    #   theme(
-    #     axis.text.x = element_blank(),
-    #     legend.position = "top",
-    #     axis.title.y = element_text(size = 12),
-    #     axis.title.x = element_text(size = 12),
-    #     axis.text.y = element_text(size = 12)
-    #   ) +
-    #   scale_y_reverse(n.breaks=10)
 
 
 
@@ -511,7 +512,7 @@ plot.BPSI = function(BPSI_result, ..., category = "BPSI"){
 
 print.BPSI = function(BPSI_result, ...){
   obj = BPSI_result
-  message("==> Considering an intensity of ", attr(obj[[1]], "control") *100,'%, here are the selected candidates:')
+  message("==> Considering an intensity of ", attr(obj[[1]], "control") *100,'%, here are the candidates selected:')
   obj=obj[[1]]
   obj$gen=rownames(obj)
   rownames(obj) = NULL
